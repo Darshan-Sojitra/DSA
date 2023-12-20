7204a95e73dcd471db5c6620d08bc0fcb1db702d
Darshan-Sojitra <darshansojitra2003@gmail.com>
2023-12-20 21:38:42 +0530

    " arrays more problems

---
+++ arrrays/concatenation.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class concatenation {
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 1 };
+        int[] ans = getConcatenation(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+}
